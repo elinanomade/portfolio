3bf80fa1b57bf0632758f0d66c4df5c6923fd24b
elinanomade <elinanomad@gmail.com>
2023-05-11 22:52:38 +0200

    add carousel

---
+++ main.js
@@ -0,0 +1,19 @@
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+const carouselItems = document.querySelector(".carousel-items");
+const carouselItemWidth = document.querySelector(".carousel-item").clientWidth;
+let currentPosition = 0;
+
+prevButton.addEventListener("click", () => {
+  if (currentPosition < 0) {
+    currentPosition += carouselItemWidth;
+    carouselItems.style.transform = `translateX(${currentPosition}px)`;
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentPosition > -carouselItemWidth * 2) {
+    currentPosition -= carouselItemWidth;
+    carouselItems.style.transform = `translateX(${currentPosition}px)`;
+  }
+});
